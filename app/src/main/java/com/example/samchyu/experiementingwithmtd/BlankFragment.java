4961c748dfaaf14777f6008663a257e71adbc7f6
Michael Vitucci <michaelvitucci1@gmail.com>
2018-04-24 22:26:10 -0500

    Added map.

--- app/src/main/java/com/example/samchyu/experiementingwithmtd/BlankFragment.java
+++ app/src/main/java/com/example/samchyu/experiementingwithmtd/BlankFragment.java
@@ -9,9 +9,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
@@ -24,12 +27,13 @@ public class BlankFragment extends Fragment implements OnMapReadyCallback{
         // Required empty public constructor
     }
 
+    private GoogleMap mMap;
     SupportMapFragment mapFragment;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_blank, container, false);
+        View map = inflater.inflate(R.layout.fragment_blank, container, false);
         mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment == null) {
             FragmentManager manager = getFragmentManager();
@@ -38,11 +42,19 @@ public class BlankFragment extends Fragment implements OnMapReadyCallback{
             transaction.replace(R.id.map, mapFragment).commit();
         }
         mapFragment.getMapAsync(this);
-        return v;
+        return map;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+        LatLng busStop = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(busStop).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(busStop));
+
     }
+
+        public void setMapType(int type) {
+        mMap.setMapType(type);
+        }
 }
